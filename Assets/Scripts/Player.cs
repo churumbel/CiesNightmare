1235c2ef7fa535908b0d6b39b2c5c880e1841e93
Diana <dianaevelinperson@gmail.com>
2025-04-30 13:06:21 +0200

    Gaviota movimiento trigger

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    //[SerializeField] private float bound = 4.5f;
+    private float upperBound=4;
+    private float lowerBound= -3.5f;
+
+    // Posición inicial de la gaviota
+    private Vector2 startPos; 
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+    }
+    void PlayerMovement()
+    {
+        // Para que la gaviota se mueva en el eje vertical
+        float moveInput = Input.GetAxisRaw("Vertical"); 
+
+        Vector2 playerPosition = transform.position;
+        // Limitamos el movimiento en el eje Y
+        playerPosition.y = Mathf.Clamp(playerPosition.y + moveInput * speed * Time.deltaTime, lowerBound, upperBound);
+        transform.position = playerPosition;
+    }
+
+    public void ResetPlayer()
+    {
+        // Posición inicial de la gaviota
+        transform.position = startPos; 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Turist")) // Si colisionamos con un turista
+        {
+            Debug.Log("Colisión con turista"); 
+            //GameManager.Instance.GanarOjota();
+        }
+
+        if (collision.CompareTag("Politician")) // Si colisionamos con un turista
+        {
+            Debug.Log("Colisión con político");
+            //Si toca al politico debería perder una vida, si toca suelo, debería perder una vida
+            //si la chancla toca al político, debería destruir al proyecto de ley
+            //Destroy(collision.gameObject); // Lo destruimos
+            //GameManager.Instance.PerderLife();
+        }
+    }
+
+
+
+}

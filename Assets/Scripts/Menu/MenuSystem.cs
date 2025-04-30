50532bbeb790f55b74dcf39251ec23cfe2e6605c
Diana <dianaevelinperson@gmail.com>
2025-04-30 13:41:54 +0200

    Menu de inicio

---
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+}
